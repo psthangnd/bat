572c6230dd59356f817466a8efdf104bf375e064
psthangnd <n.d.thang1987@gmail.com>
2016-08-08 09:00:22 +0700

    Google Mail API: build, send email (test)

--- src/main/java/cowell/vn/constant/GoogleConstant.java
+++ src/main/java/cowell/vn/constant/GoogleConstant.java
@@ -3,13 +3,16 @@ package cowell.vn.constant;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.api.services.gmail.GmailScopes;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
 public class GoogleConstant {
 	public static String CREDENTIALS_PATH = ".credentials/sheets.googleapis.com-bat.json";
 	
-	public static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS);
+	public static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS, GmailScopes.GMAIL_SEND);
 	
 	public static final int N_TIME_TRY = 3;
+	
+	public static final String USER_ID_DEFAULT = "me";
 	
 }
